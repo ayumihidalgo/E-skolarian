ca34612c0ae092773ad20ef5bcd36e458a251087
view10cm <redaaronpnegrite@iskolarngbayan.pup.edu.ph>
2025-04-21 15:32:54 +0800

    REFACTOR: Additional Commands in the Logout Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LogoutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/logout',[LogoutController::class,'logout'])->name('logout');
