2412cd0444e98b87442ee9e1150e9f5e7a3d2636
enki-22 <enki.prince.alava@gmail.com>
2025-04-28 16:50:46 +0800

    feat: added a redirect link in the admin dashboard for the calendar, fixed some UI style and added a create event button

--- resources/views/components/adminNavigation.blade.php
+++ resources/views/components/adminNavigation.blade.php
@@ -18,12 +18,19 @@
 
             <!-- Navigation Links -->
             <nav class="space-y-4 text-lg font-[Marcellus_SC] mt-6">
-                @foreach ([['Home', 'account.svg'], ['Review', 'archive.svg'], ['Archive', 'tracker.svg'], ['Calendar', 'calendar.svg'], ['Settings', 'settings.svg']] as [$label, $icon])
-                    <a href="#" class="flex items-center space-x-3 hover:text-yellow-400 transition duration-200">
-                        <img src="{{ asset("images/$icon") }}" class="h-6 w-6" alt="{{ $label }} Icon">
-                        <span class="sidebar-text">{{ $label }}</span>
-                    </a>
-                @endforeach
+    @foreach ([
+        ['Home', 'account.svg', '#'], 
+        ['Review', 'archive.svg', '#'], 
+        ['Archive', 'tracker.svg', '#'], 
+        ['Calendar', 'calendar.svg', '/calendar'], 
+        ['Settings', 'settings.svg', '#']
+    ] as [$label, $icon, $url])
+        <a href="{{ $url }}" class="flex items-center space-x-3 hover:text-yellow-400 transition duration-200 {{ request()->is(ltrim($url, '/')) ? 'text-yellow-400' : '' }}">
+            <img src="{{ asset("images/$icon") }}" class="h-6 w-6" alt="{{ $label }} Icon">
+            <span class="sidebar-text">{{ $label }}</span>
+        </a>
+    @endforeach
+
 
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
