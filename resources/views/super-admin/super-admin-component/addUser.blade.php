26479a5b5bd1b3273f6d5e19e35f91de2e68b0d8
luckyystar <casubhalucky5@gmail.com>
2025-05-02 13:37:31 +0800

    CHORE: Add role_name column in users table and fix the role column

--- resources/views/super-admin/super-admin-component/addUser.blade.php
+++ resources/views/super-admin/super-admin-component/addUser.blade.php
@@ -1,7 +1,7 @@
 <!-- Add User Component -->
 <div class="bg-white rounded-[25px] shadow-xl w-full max-w-lg relative z-50">
     <!-- Close button -->
-    <button id="closeAddUserModalBtn" 
+    <button id="closeAddUserModalBtn"
                 class="absolute top-4 right-4 text-gray-500 hover:text-[#7A1212] transition-colors duration-200">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -13,36 +13,36 @@
         <form id="addUserForm">
             <div class="mb-4">
                 <label for="username" class="block text-sm font-medium mb-2 text-gray-700">Username</label>
-                <input type="text" 
-                    name="username" 
-                    id="username" 
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#7A1212]" 
-                    required 
+                <input type="text"
+                    name="username"
+                    id="username"
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#7A1212]"
+                    required
                     placeholder="Complete Organization Name or Admin Name">
             </div>
-            
+
             <div class="mb-4">
                 <label for="email" class="block text-gray-700 text-sm font-medium mb-2">Email</label>
-                <input type="email" 
-                    name="email" 
-                    id="email" 
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#7A1212]" 
-                    required 
+                <input type="email"
+                    name="email"
+                    id="email"
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#7A1212]"
+                    required
                     placeholder="Enter user's email">
             </div>
-            
+
             <div class="mb-6">
-                <label for="role" class="block text-sm font-medium mb-2 text-gray-700">Role</label>
-                <select name="role" 
-                        id="role" 
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#7A1212]" 
+                <label for="role_name" class="block text-sm font-medium mb-2 text-gray-700">Role</label>
+                <select name="role_name"
+                        id="role_name"
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#7A1212]"
                         required>
                     <option value="" disabled selected>Choose a role for the user</option>
-                    <option value="organization">Academic Organization</option>
-                    <option value="organization">Non-Academic Organization</option>
-                    <option value="admin">Student Services</option>
-                    <option value="admin">Academic Services</option>
-                    <option value="admin">Administrative Services</option>
+                    <option value="Academic Organization">Academic Organization</option>
+                    <option value="Non-Academic Organization">Non-Academic Organization</option>
+                    <option value="Student Services">Student Services</option>
+                    <option value="Academic Services">Academic Services</option>
+                    <option value="Administrative Services">Administrative Services</option>
                 </select>
             </div>
             <p class="text-sm text-gray-500 mb-6 text-center">The reset password link will be sent to the user via email.</p>
