cc9f98e1272e19a9001fb09fe017e42d660f71ea
CarlStephenTheDev <carlstephenhgarcia@iskolarngbayan.pup.edu.ph>
2025-05-04 00:55:15 +0800

    Do not merge

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{ asset('images/officialLogo.svg') }}" type="image/svg+xml">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -12,12 +13,13 @@
         rel="stylesheet">
 
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
-
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
 
-    <title> ESKOLARIAN </title>  
+    <!--title-->
+    <title> ESKOLARIAN </title> 
 </head>
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    
 <body>
     <main>
         @yield('content')
