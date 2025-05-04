2f93bd5d02ea0ac0a68b51f110d8550ca6d71155
nathanielbugna <nathanielrbugna@iskolarngbayan.pup.edu.ph>
2025-05-04 19:47:25 +0800

    FEAT DSM5-30 : Connected UI with backend via API

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    protected $table = 'submitted_documents';
+
+    protected $fillable = [
+        'doc_receiver',
+        'subject',
+        'doc_type',
+        'summary',
+        'event_start_date',
+        'event_end_date',
+        'file_path',
+    ];
 }
---
+++ database/migrations/2025_05_03_031958_create_submitted_documents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('submitted_documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('doc_receiver');
+            $table->string('subject');
+            $table->string('doc_type');
+            $table->text('summary')->nullable();
+            $table->date('eventStartDate')->nullable();
+            $table->date('eventEndDate')->nullable();
+            $table->string('file_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submitted_documents');
+    }
+};
