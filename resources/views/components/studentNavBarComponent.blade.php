bc55c47bb6c246ebdb7f4a88fa468648539a15f0
view10cm <redaaronpnegrite@iskolarngbayan.pup.edu.ph>
2025-05-05 14:01:07 +0800

    STVC2-59 FEAT: Student Navigation Bar Component

---
+++ resources/views/components/studentNavBarComponent.blade.php
@@ -0,0 +1,9 @@
+<!-- Navigation Bar -->
+<div>
+    <nav class="w-full bg-[#4d0F0F] h-[10%] p-4 text-white flex justify-end items-center space-x-6">
+        <x-general-components.notification />
+        <div>
+            <a href="#" class="font-semibold">Student</a>
+        </div>
+    </nav>
+</div>
