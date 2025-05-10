e5223ea7ee270694e0a17c8493f6a19f7f5dd815
view10cm <redaaronpnegrite@iskolarngbayan.pup.edu.ph>
2025-05-10 17:08:29 +0800

    STVC2-88 FEAT: Display Student Record submissions

--- app/Http/Controllers/StudentTrackerController.php
+++ app/Http/Controllers/StudentTrackerController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SubmittedDocument;
 
 class StudentTrackerController extends Controller
 {
     public function viewStudentTracker()
     {
-        return view('student.studentTracker');
+     $records = SubmittedDocument::paginate('15');
+            return view('student.studentTracker', compact('records'));
     }
 }
