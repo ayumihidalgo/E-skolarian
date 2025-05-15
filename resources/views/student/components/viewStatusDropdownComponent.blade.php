24ab7534277f84459864c35d711af3c961357802
view10cm <redaaronpnegrite@iskolarngbayan.pup.edu.ph>
2025-05-15 22:56:16 +0800

    STVC2-144 FIX: Status Dropdown Changes

--- resources/views/student/components/viewStatusDropdownComponent.blade.php
+++ resources/views/student/components/viewStatusDropdownComponent.blade.php
@@ -1,11 +1,11 @@
-<div class="relative" style="position: relative; top: -5px;">
+<div class="relative" style="position: relative;">
     <select
-        class="appearance-none border border-gray-300 rounded-full px-4 py-2 bg-[#7A1212] text-white font-bold focus:outline-none focus:ring-2 focus:ring-[#DAA512] cursor-pointer hover:bg-white hover:text-[#DAA512] transition ease-in duration-200">
-        <option value="" disabled selected class="bg-white text-black">Status</option>
+        class="appearance-none border border-gray-300 rounded-full px-4 py-2 bg-[#7A1212] text-white font-bold focus:outline-none focus:ring-2 focus:ring-[#4D0F0F] cursor-pointer hover:bg-white hover:text-[#4D0F0F] transition ease-in duration-200">
+        <option value="" disabled selected hidden>Status</option>
         <option value="Approved" class="bg-white text-black">Approved</option>
         <option value="Rejected" class="bg-white text-black">Resubmit</option>
         <option value="Under Review" class="bg-white text-black">Under Review</option>
-        <option value="Pending" class = "bg-white text-black">Pending</option>
+        <option value="Pending" class="bg-white text-black">Pending</option>
         <option value="Rejected" class="bg-white text-black">Rejected</option>
     </select>
     <img src="{{ asset('images/dropdownIcon.svg') }}" alt="Dropdown Icon"
